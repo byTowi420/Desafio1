f1e430111428913473da0f6e3c1cd735be5164fc
Durluc1324 <lucianorodriguezpaven@gmail.com>
2025-08-14 19:20:08 -0300

    Implementación de la operación adición

--- src/Program/Program.cs
+++ src/Program/Program.cs
@@ -3,6 +3,15 @@ Console.WriteLine(Substraction.Substract(10, 1));
 Console.WriteLine(Multiplication.Multiply(3, 3));
 Console.WriteLine(Division.Divide(18, 2));
 
+// Esta clase implementa la operación de adición 
+public class Addition
+{
+    public static int Add(int a, int b)
+    {
+        return a + b;
+    }
+}
+
 
 //Esta clase implementa la operación Multiplicación
 public class Multiplication
