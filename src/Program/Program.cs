f8c34356ee430584a41eed65fa6a3e3def96794d
byTowi420 <j.torrescomas@gmail.com>
2025-08-14 11:59:59 -0300

    Comentario multiplicacion agregado

--- src/Program/Program.cs
+++ src/Program/Program.cs
@@ -3,6 +3,8 @@ Console.WriteLine(Substraction.Substract(10, 1));
 Console.WriteLine(Multiplication.Multiply(3, 3));
 Console.WriteLine(Division.Divide(18, 2));
 
+
+//Esta clase implementa la operación Multiplicación
 public class Multiplication
 {
     public static int Multiply(int a, int b)
